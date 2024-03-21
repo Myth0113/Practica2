eee3ac3137a13ba6fae4a1b5b7aee9fc1d308a78
MAPUGA05 <161206150+MAPUGA05@users.noreply.github.com>
2024-03-21 12:36:06 +0100

    Update Escala.java

--- SuarezBatlloriAdriaPuigGallesMarti/src/prog2/model/Escala.java
+++ SuarezBatlloriAdriaPuigGallesMarti/src/prog2/model/Escala.java
@@ -13,4 +13,9 @@ public class Escala extends AccesDesnivell{
     public Escala(LlistaVies llista, String _nom, boolean acces, boolean _estat, int _desnivell) {
         super(llista, _nom, acces, _estat, _desnivell);
     }
+
+    @Override
+    public String toString() {
+        return ("Acces de tipus Escala, " + super.toString());
+    }
 }
